cc0164199d413210184a915afb0db57055091b27
Jackson Tian <shyvo1987@gmail.com>
2013-04-14 18:31:30 +0800

    Add menu

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
 var wechat = process.env.WEIXIN_COV ? require('./lib-cov/wechat') : require('./lib/wechat');
 wechat.List = process.env.WEIXIN_COV ? require('./lib-cov/list') : require('./lib/list');
+wechat.API = process.env.WEIXIN_COV ? require('./lib-cov/common') : require('./lib/common');
 module.exports = wechat;
